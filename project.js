29b71199920e82e9c4f008145784a54bba3629ba
Sterling Hopkins <sterlingxhopkins@gmail.com>
2022-02-28 21:51:50 -0500

    Added function for random city generator

--- project.js
+++ project.js
@@ -2,39 +2,32 @@
 
 // "Hello world"
 
-let listOfCities = ["Los Angeles", "Orlando", "Toronto", "Seattle", "Dallas", "Colorado", "Phoenix"];
+let city = ["Los Angeles", "Orlando", "Toronto", "Seattle", "Dallas", "Colorado", "Phoenix"];
 
-let listOfRestaurants = ["Yard House", "P.F. Chang's", "Bonefish Grill", "Maggiano's Little Italy", "The Cheesecake Factory", "Pappadeaux Seafood Kitchen", "Benihana"];
-
-let listOfTransportation = ["Bus", "Train", "Owned Car", "Rental Car", "Bike", "Motorcycle"];
-
-let listofEntertainment = ["Music Concert", "Amusement Park", "City Tour", "Wine Tasting", "Mueseum", "Comedy Show", "Night Club"]
-
-function randomGenerator(myArray){
-    let randomChoice = myArray[Math.floor(Math.random() * myArray.length)]
-    return randomChoice
+function cityGenerator(selectionOfCity){
+    return selectionOfCity[Math.floor(Math.random()*selectionOfCity.length)]
 }
 
-function cityGenerator(){
-    let userCityHappy = false
+let citySelection = cityGenerator(city);
 
-    while(userCityHappy === false){
-        let randomCity = randomGenerator(listOfCities)
+let restaurant = ["Yard House", "P.F. Chang's", "Bonefish Grill", "Maggiano's Little Italy", "The Cheesecake Factory", "Pappadeaux Seafood Kitchen", "Benihana"];
 
-        let userResponse = prompt(`Your randomly selected city is ${randomCity}. Are you happy with this selection?`)
+function restaurantGenerator(selectionOfRestaurant){
+    return selectionOfCity[Math.floor(Math.random()*selectionOfCity.length)]
+}
 
-        if(userResponse === 'yes'){
-            alert(`See you in ${randomCity}`)
-            userCityHappy = true
-        }else if(userResponse === 'no'){
-            userCityHappy = false   
-        }switch(userResponse){
-            case 'yes':
-                console.log('City is confirmed');
-                break;
-            case 'no':
-                console.log('Please try again');
-                break;
-    }
-    }
-}cityGenerator()
+let restaurantSelection = restaurantGenerator(restaurant);
+
+let transportation = ["Bus", "Train", "Owned Car", "Rental Car", "Bike", "Motorcycle"];
+
+function vehicleGenerator(selectionOfVehicle){
+    selectionOfVehicle[Math.floor(Math.random()*selectionOfVehicle.length)]
+}
+
+let vehicleSelection = vehicleGenerator(transportation);
+
+let entertainment = ["Music Concert", "Amusement Park", "City Tour", "Wine Tasting", "Mueseum", "Comedy Show", "Night Club"]
+
+function entertainmentGenerator(selectionOfEntertainment){
+    selectionOfEntertainment = myArray[Math.floor(Math.random() * myArray.length)]
+}
